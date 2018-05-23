14efc431e268d257c6945d957c8d7328c76759bb
Bayue Walker <cbayue@gmail.com>
2018-05-24 00:35:00 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2801,7 +2801,7 @@ bool InitBlockIndex() {
             block.nNonce   = 388757645;
         }
 
-if (true && block.GetHash() != hashGenesisBlock)
+if (false && block.GetHash() != hashGenesisBlock)
         {
             printf("Searching for genesis block...\n");
             // This will figure out a valid hash and Nonce if you're
